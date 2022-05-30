6d6fdf7554b0c45b4f983d91fc123bb43c47e97a
cottonterrorrr <cottonterrorrr@gmail.com>
2022-05-30 21:22:56 +0200

    Added High Score feature
    Added High Score feature

--- snake/main.py
+++ snake/main.py
@@ -35,15 +35,15 @@ while game_is_on:
 
     # Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or  snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collision with tail
     for segment in snake.segments[1:]:
         # if head collides with any segment in the tail:
         # trigger game_over
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
