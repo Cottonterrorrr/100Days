21b4316902b43a9933d7cbb9f601c57103f83a30
cottonterrorrr <cottonterrorrr@gmail.com>
2022-06-30 22:47:59 +0200

    Create Pomodoro app
    Create Pomodoro app

--- snake/scoreboard.py
+++ snake/scoreboard.py
@@ -9,7 +9,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt", 'r') as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -23,6 +24,9 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))
+                #file.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
